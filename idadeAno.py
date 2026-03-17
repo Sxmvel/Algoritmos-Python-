1178390a3e8f65c048733032e9b08c597379c78c
Sxmvel <samuelres444@gmail.com>
2026-03-17 15:21:28 -0300

    Script simples de nome e idade

--- idadeAno.py
+++ idadeAno.py
@@ -1,10 +1,2 @@
 
 
-
-nome =  input("Informe seu nome completo: ")
-idade_texto = input("Quantos anos você tem?")
-
-idade = int(idade_texto)
-ano_nascimento = 2026 - idade
-
-print("Olá, " + nome + "! sua idade é aproximadamente ", ano_nascimento)
